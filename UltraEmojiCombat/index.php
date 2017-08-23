ff3477ab8b7e60e61a60ead481a20ee0b0ceb2c6
Fabio <fabio.britto15@gmail.com>
2017-08-22 21:04:47 -0300

    finalizaçãoAula08

--- UltraEmojiCombat/index.php
+++ UltraEmojiCombat/index.php
@@ -8,6 +8,7 @@
         <pre>
         <?php
         require_once 'Lutador.php';
+        require_once 'Luta.php';
         $l = array();
         $l[0] = new Lutador("Pretty Boy", "França", 30, 1.35,68.9,11,2,1);
         $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
@@ -16,9 +17,10 @@
         $l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
         $l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81,  105.1, 12, 2, 4);
         
-        $l[3]->perderLuta();
-        $l[3]->apresentar();
-        $l[3]->status();
+       $UEC01 = new Luta;
+       $UEC01->marcarLuta($l[4], $l[5]);
+       $UEC01->lutar();
+       
 
         
         ?>
